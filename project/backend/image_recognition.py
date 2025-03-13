e844d9259acf7085cd05a87ab387bf715fd09791
KorolOrol <muxanuk.67@gmail.com>
2025-03-13 16:52:00 +0700

    feat: добавить функцию для отправки изображений на распознавание

---
+++ project/backend/image_recognition.py
@@ -0,0 +1,32 @@
+import requests
+
+def send_image_recognition_request(image, url):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "model": "gemma-3-4b-it",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "ты должен давать ключевые слова для поиска товара на маркетплейсах по картинкам, которые тебе скидывают. ничего больше не добавляй"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": image
+                        }
+                    }
+                ]
+            }
+        ],
+        "temperature": 0.7,
+        "max_tokens": -1,
+        "stream": False
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    return response.json()['choices'][0]['message']['content']
